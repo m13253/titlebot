c49ddacb601e6fa8250de1741b1accedceced6ae
Star Brilliant <m13253@hotmail.com>
2012-08-10 13:02:05 +0800

    Initial commit

--- titlebot.py
+++ titlebot.py
@@ -56,7 +56,7 @@ while not quiting:
                                     wbuf=h.read(4096)
                                     if wbuf.find("<title>")!=-1:
                                         title=wbuf.split("<title>")[1].split("</title>")[0]
-                                        title=html_parser.unescape(unicode(title, "utf-8")).encode("utf-8").replace("\r", "").replace("\n", " ")
+                                        title=html_parser.unescape(title.decode("utf-8", "replace").encode("utf-8", "replace").replace("\r", "").replace("\n", " ")
                                         s.send("PRIVMSG %s :⇪标题: %s\r\n" % (CHAN, title))
                                 else:
                                     if "Content-Length" in h.info():
