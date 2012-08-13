6fe451b6658cf591239266cc73b2576fb87467f1
Star Brilliant <m13253@hotmail.com>
2012-08-13 23:07:08 +0800

    Initial commit

--- titlebot.py
+++ titlebot.py
@@ -52,7 +52,7 @@ while not quiting:
                     for w in content.split():
                         if w.startswith("http:") or w.startswith("https:"):
                             opener=urllib2.build_opener()
-                            opener.addheaders = [("Accept-Charset", "utf-8, iso-8859-1"), ("Accept-Language", "zh-cn, zh-hans, zh-tw, zh-hant, zh, en-us, en-gb, en"), ("Range", "bytes=0-4095"), ("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.1 (KHTML, like Gecko) Safari/537.1"), ("X-Forwarded-For", "10.2.0.101"), ("X-moz", "prefetch"), ("X-Prefetch", "yes")]
+                            opener.addheaders = [("Accept-Charset", "utf-8, iso-8859-1"), ("Accept-Language", "zh-cn, zh-hans, zh-tw, zh-hant, zh, en-us, en-gb, en"), ("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.1 (KHTML, like Gecko) Safari/537.1"), ("X-Forwarded-For", "10.2.0.101"), ("X-moz", "prefetch"), ("X-Prefetch", "yes")]
                             h=opener.open(w)
                             if h.code==200 or h.code==206:
                                 if not "Content-Type" in h.info() or h.info()["Content-Type"].split(";")[0]=="text/html":
