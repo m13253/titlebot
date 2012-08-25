aa685a343de8e86612119027a6cd9bbf499a8466
Star Brilliant <m13253@hotmail.com>
2012-08-25 15:02:09 +0800

    Rewrite using libirc

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ titlebot
 
 An IRC robot that parses URLs and post the title of that webpage
 
-change HOST PORT NICK IDENT REALNAME CHAN in the source before using.
+You need to grab libirc.py from https://github.com/m13253/libirc before using.
+
+change HOST PORT NICK IDENT REALNAME CHANS in the source before using.
--- titlebot.py
+++ titlebot.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python2
 # coding: utf-8
 
+import os
 import sys
 import socket
 import string
 import urllib2
 import HTMLParser
+
+import libirc
 
 HOST="irc.freenode.net"
 PORT=6667
@@ -28,13 +31,12 @@ def ParseURL(s):
         else:
             return s[min(http_idx, https_idx):]
 
-readbuffer=""
-s=socket.socket()
-s.connect((HOST, PORT))
-s.send("NICK %s\r\n" % NICK)
-s.send("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME))
+c=libirc.IRCConnection()
+c.connect(HOST, PORT)
+c.setnick(NICK)
+c.setuser(IDENT, REALNAME)
 for CHAN in CHANS:
-    s.send("JOIN :%s\r\n" % CHAN)
+    c.join(CHAN)
 CHAN=CHANS[0]
 socket.setdefaulttimeout(10)
 
@@ -42,52 +44,48 @@ html_parser=HTMLParser.HTMLParser()
 
 quiting=False
 while not quiting:
-    readbuffer=readbuffer+s.recv(1024)
-    temp=string.split(readbuffer, "\n")
-    readbuffer=temp.pop()
-    for line in temp:
-        try:
-            print line
-            line=string.rstrip(line)
-            sline=string.split(line)
-            if sline[0]=="PING":
-                s.send("PONG %s\r\n" % sline[1])
-            elif sline[1]=="PRIVMSG":
-                rnick=sline[0][1:].split("!")[0]
-                CHAN=sline[2]
-                if line.find(" PRIVMSG %s :" % NICK)!=-1:
-                    if line.split(" PRIVMSG %s :" % NICK)[1]=="Get out of this channel!": # A small hack
-                        s.send("QUIT :Client Quit\r\n")
-                        quiting=True
-                    else:
-                        s.send("PRIVMSG %s :%s: 我不接受私信哦。\r\n" % (rnick, rnick))
-                else:
-                    content=line.split(" PRIVMSG %s :" % CHAN)[1]
-                    for w in content.split():
-                        w=ParseURL(w)
-                        if w:
-                            opener=urllib2.build_opener()
-                            opener.addheaders = [("Accept-Charset", "utf-8, iso-8859-1"), ("Accept-Language", "zh-cn, zh-hans, zh-tw, zh-hant, zh, en-us, en-gb, en"), ("Range", "bytes=0-16383"), ("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.1 (KHTML, like Gecko) Safari/537.1"), ("X-Forwarded-For", "10.2.0.101"), ("X-moz", "prefetch"), ("X-Prefetch", "yes")]
-                            h=opener.open(w)
-                            if h.code==200 or h.code==206:
-                                if not "Content-Type" in h.info() or h.info()["Content-Type"].split(";")[0]=="text/html":
-                                    wbuf=h.read(16384)
-                                    if wbuf.find("<title>")!=-1:
-                                        title=wbuf.split("<title>")[1].split("</title>")[0]
-                                        title=html_parser.unescape(title.decode("utf-8", "replace")).encode("utf-8", "replace").replace("\r", "").replace("\n", " ").strip()
-                                        s.send("PRIVMSG %s :⇪标题: %s\r\n" % (CHAN, title))
-                                    else:
-                                        s.send("PRIVMSG %s :⇪无标题网页\r\n" % CHAN)
+    if not c.sock:
+        quiting=True
+        time.sleep(10)
+        sys.stderr.write("Restarting...\n")
+        os.execlp("python2", "python2", __file__)
+        break
+    try:
+        line=c.parse(block=True)
+        if line["cmd"]=="PRIVMSG":
+            if line["dest"]==NICK:
+                if line["msg"]==u"Get out of this channel!": # A small hack
+                    c.quit(u"%s asked to leave." % line["nick"])
+                    quiting=True
+            else:
+                for w in line["msg"].split():
+                    w=ParseURL(w)
+                    if w:
+                        opener=urllib2.build_opener()
+                        opener.addheaders = [("Accept-Charset", "utf-8, iso-8859-1"), ("Accept-Language", "zh-cn, zh-hans, zh-tw, zh-hant, zh, en-us, en-gb, en"), ("Range", "bytes=0-16383"), ("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.1 (KHTML, like Gecko) Safari/537.1"), ("X-Forwarded-For", "10.2.0.101"), ("X-moz", "prefetch"), ("X-Prefetch", "yes")]
+                        h=opener.open(w)
+                        if h.code==200 or h.code==206:
+                            if not "Content-Type" in h.info() or h.info()["Content-Type"].split(";")[0]=="text/html":
+                                wbuf=h.read(16384)
+                                if wbuf.find("<title>")!=-1:
+                                    title=wbuf.split("<title>")[1].split("</title>")[0]
+                                    title=html_parser.unescape(title.decode("utf-8", "replace")).replace("\r", "").replace("\n", " ").strip()
+                                    c.say(CHAN, u"⇪标题: %s" % title)
                                 else:
-                                    if "Content-Range" in h.info():
-                                        s.send("PRIVMSG %s :⇪文件类型: %s, 文件大小: %s 字节\r\n" % (CHAN, h.info()["Content-Type"], h.info()["Content-Range"].split("/")[1]))
-                                    elif "Content-Length" in h.info():
-                                        s.send("PRIVMSG %s :⇪文件类型: %s, 文件大小: %s 字节\r\n" % (CHAN, h.info()["Content-Type"], h.info()["Content-Length"]))
-                                    else:
-                                        s.send("PRIVMSG %s :⇪文件类型: %s\r\n" % (CHAN, h.info()["Content-Type"]))
+                                    c.say(CHAN, u"⇪无标题网页")
                             else:
-                                s.send("PRIVMSG %s :⇪HTTP %d 错误\r\n" % (CHAN, h.code))
-        except Exception as e:
-            s.send("PRIVMSG %s :哎呀，%s 好像出了点问题: %s\r\n" % (CHAN, NICK, e))
+                                if "Content-Range" in h.info():
+                                    s.say(CHAN, u"⇪文件类型: %s, 文件大小: %s 字节\r\n" % (h.info()["Content-Type"], h.info()["Content-Range"].split("/")[1]))
+                                elif "Content-Length" in h.info():
+                                    c.say(CHAN, u"⇪文件类型: %s, 文件大小: %s 字节\r\n" % (h.info()["Content-Type"], h.info()["Content-Length"]))
+                                else:
+                                    c.say(CHAN, u"⇪文件类型: %s\r\n" % h.info()["Content-Type"])
+                        else:
+                            c.say(CHAN, u"⇪HTTP %d 错误\r\n" % h.code)
+    except Exception as e:
+        c.say(CHAN, u"哎呀，%s 好像出了点问题: %s" % (NICK, e))
+    except socket.err as e:
+        sys.stderr.write("Error: %s\n", e)
+        c.quit("Network error.")
 
 # vim: et ft=python sts=4 sw=4 ts=4
