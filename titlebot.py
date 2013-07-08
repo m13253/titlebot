2e3948a03a225fc3d111e3e3131c30e52be5e4bc
Tom Li <biergaizi2009@gmail.com>
2013-07-08 12:50:47 +0800

    移出 Headers

--- titlebot.py
+++ titlebot.py
@@ -19,6 +19,15 @@ IDENT="titlebot"
 REALNAME="titlebot"
 CHANS=["##Orz"]
 
+HEADERS = [("Accept-Charset", "utf-8, iso-8859-1"),
+           ("Accept-Language", "zh-cn, zh-hans, zh-tw, zh-hant, zh, en-us, en-gb, en"),
+           ("Range", "bytes=0-16383"),
+           ("User-Agent", "Mozilla/5.0 (compatible; Titlebot; like IRCbot; +https://github.com/m13253/titlebot)"),
+           ("X-Forwarded-For", "10.2.0.101"),
+           ("X-moz", "prefetch"),
+           ("X-Prefetch", "yes"),
+           ("X-Requested-With", "Titlebot")]
+
 def pickupUrl(text):
     """Return a vaild URL from a string"""
 
@@ -74,7 +83,7 @@ while running:
                         if re.match("https?:/*git.io(/|$)", w): # Fix buggy git.io
                             continue
                         opener=urllib2.build_opener()
-                        opener.addheaders = [("Accept-Charset", "utf-8, iso-8859-1"), ("Accept-Language", "zh-cn, zh-hans, zh-tw, zh-hant, zh, en-us, en-gb, en"), ("Range", "bytes=0-16383"), ("User-Agent", "Mozilla/5.0 (compatible; Titlebot; like IRCbot; +https://github.com/m13253/titlebot)"), ("X-Forwarded-For", "10.2.0.101"), ("X-moz", "prefetch"), ("X-Prefetch", "yes"), ("X-Requested-With", "Titlebot")]
+                        opener.addheaders = HEADERS
                         h=opener.open(w.encode("utf-8", "replace"))
                         if h.code==200 or h.code==206:
                             if not "Content-Type" in h.info() or h.info()["Content-Type"].split(";")[0]=="text/html":
